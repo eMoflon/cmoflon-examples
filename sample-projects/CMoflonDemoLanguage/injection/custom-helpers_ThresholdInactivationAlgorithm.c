b1dd0f6dc6b9d8e5a4ed03973c8b529a11a85d1b
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-11-09 11:20:23 +0100

    Add sample algorithm for foreach nodes

---
+++ sample-projects/CMoflonDemoLanguage/injection/custom-helpers_ThresholdInactivationAlgorithm.c
@@ -0,0 +1,8 @@
+// Helper definitions for 'ThresholdInactivationAlgorithm'.
+static EDouble thresholdInactivationAlgorithm_getMaxWeight(THRESHOLDINACTIVATIONALGORITHM_T* _this) {
+  return _this->maxWeight;
+}
+
+static NODE_T* thresholdInactivationAlgorithm_getNode(THRESHOLDINACTIVATIONALGORITHM_T* _this) {
+  return _this->node;
+}
